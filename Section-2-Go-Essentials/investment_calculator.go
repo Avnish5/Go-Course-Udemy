54ec71c1a49a84dc9e82d79efdba6ba75550658a
Avnish Kumar <avnein4988@gmail.com>
2024-10-16 10:23:33 +0530

    completed the section-2 and its assignments

--- Section-2-Go-Essentials/investment_calculator.go
+++ Section-2-Go-Essentials/investment_calculator.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
-
-func add(a int, b int) int {
-	if a < 0 || b < 0 {
-		panic("cannot add negative numbers")
-	}
-	return a + b
-}
 func main() {
 
-	fmt.Println("Adding 5 and 3:", add(5, 3))
-
-	// This will trigger a panic
-	fmt.Println("Adding -1 and 4:", add(-1, 4))
+	profitCalulator()
 
 }
--- Section-2-Go-Essentials/profit_calculator.go
+++ Section-2-Go-Essentials/profit_calculator.go
@@ -1,26 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func storeResult(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\n Ratio: %.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
 
 func profitCalulator() {
-	var revenue float64
-	var expense float64
-	var taxRate float64
 
-	revenue = takeInput("Revenue: ")
-	expense = takeInput("Expense: ")
-	taxRate = takeInput("Tax Rate: ")
+	revenue, err1 := takeInput("Revenue: ")
+	expense, err2 := takeInput("Expense: ")
+	taxRate, err3 := takeInput("Tax Rate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Print(err1)
+		return
+	}
 
 	ebt, profit, ratio := calulate(expense, revenue, taxRate)
+	storeResult(ebt, profit, ratio)
 
 	output(ebt, profit, ratio)
 }
 
-func takeInput(infoText string) float64 {
+func takeInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("do not enter negative or 0 value")
+	}
+	return userInput, nil
 
 }
 

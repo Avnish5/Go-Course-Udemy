16ead3194c02443bceda34a4dc490c7553763955
Avnish Kumar <avnein4988@gmail.com>
2024-10-18 12:49:20 +0530

    learn about closure and anonymous function in go

---
+++ Section-8-Functions-Deep-Dive/function/function.go
@@ -0,0 +1,59 @@
+package function
+
+import "fmt"
+
+// giving custom type name to a function
+type tranformfn func(int) int
+
+func main() {
+
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 2, 3}
+
+	//gettting the new array by passing an array and function to the function
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	//getting the function
+	transformFun1 := getTransformerFunction(&numbers)
+	transformFun2 := getTransformerFunction(&moreNumbers)
+
+	//gettting the new array by passing an array and function to the function
+	tranformNumbers := transformNumbers(&numbers, transformFun1)
+	tranformMoreNumbers := transformNumbers(&moreNumbers, transformFun2)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+
+	fmt.Println(tranformNumbers)
+	fmt.Println(tranformMoreNumbers)
+
+}
+
+// it takes array and function as paramter and return the new array
+func transformNumbers(numbers *[]int, transfrom tranformfn) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transfrom(val))
+	}
+
+	return dNumbers
+}
+
+// this function returns the function based upon condition
+func getTransformerFunction(numbers *[]int) tranformfn {
+
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(val int) int {
+	return val * 2
+}
+
+func triple(val int) int {
+	return val * 3
+}
--- Section-8-Functions-Deep-Dive/main.go
+++ Section-8-Functions-Deep-Dive/main.go
@@ -2,58 +2,37 @@ package main
 
 import "fmt"
 
-// giving custom type name to a function
-type tranformfn func(int) int
-
 func main() {
+	numbers := []int{1, 2, 3}
 
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 2, 3}
+	transformed := transformNumbers(&numbers, func(val int) int {
+		return val * 2
+	})
 
-	//gettting the new array by passing an array and function to the function
+	double := createTranformer(2)
+	triple := createTranformer(3)
+
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
-	//getting the function
-	transformFun1 := getTransformerFunction(&numbers)
-	transformFun2 := getTransformerFunction(&moreNumbers)
-
-	//gettting the new array by passing an array and function to the function
-	tranformNumbers := transformNumbers(&numbers, transformFun1)
-	tranformMoreNumbers := transformNumbers(&moreNumbers, transformFun2)
-
+	fmt.Println(transformed)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
-
-	fmt.Println(tranformNumbers)
-	fmt.Println(tranformMoreNumbers)
-
 }
 
-// it takes array and function as paramter and return the new array
-func transformNumbers(numbers *[]int, transfrom tranformfn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transfrom(val))
+		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
 }
 
-// this function returns the function based upon condition
-func getTransformerFunction(numbers *[]int) tranformfn {
+func createTranformer(factor int) func(int) int {
 
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
+	return func(number int) int {
+		return number * factor
 	}
-}
-
-func double(val int) int {
-	return val * 2
-}
-
-func triple(val int) int {
-	return val * 3
 }
